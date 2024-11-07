dc10994b4fc152581d16e6c3e3fd40fae5005b5d
Edwin Marroquin devem <60857232+devem-software@users.noreply.github.com>
2024-11-07 17:56:14 -0500

    update web/scripts/new_app.js

--- web/scripts/new_app.js
+++ web/scripts/new_app.js
@@ -22,9 +22,9 @@ const maleteams = dg.get_teams("M");
 const filters = document.querySelector(".app__filters_controls");
 
 const add_filter = (data, data_optional, label_text, id, styles) => {
-  console.log(data)
+    console.log(data);
     data = Object.keys(data);
-    console.log(data)
+    console.log(data);
     filters.append(
         render.selector({
             data,
